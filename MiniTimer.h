ac1aa4455d46057ac72e91868bb583a9ffb4fcc1
vinjn <vinjn.z@gmail.com>
2013-06-17 22:09:22 +0800

    first new commit

---
+++ MiniTimer.h
@@ -0,0 +1,73 @@
+#ifndef _MINI_TIMER_H_
+#define _MINI_TIMER_H_
+
+#ifdef WIN32
+#include <windows.h>
+#else
+#include <sys/time.h>
+#endif
+ 
+//uncomment this if u have "FloWrite.h"
+//#define USING_FLO_WRITE
+
+#ifdef USING_FLO_WRITE
+#include "FloWrite.h"
+#else
+#include <stdio.h>
+#define FloWrite(str) 
+#endif
+
+#pragma comment(lib,"winmm.lib")
+
+class MiniTimer
+{
+public:
+	MiniTimer()
+	{
+		resetStartTime();
+	}
+    
+    static unsigned int getGlobalTime()
+    {
+#ifdef WIN32
+        return timeGetTime();
+#else
+        timeval tv;
+        gettimeofday(&tv, 0 );
+        return tv.tv_usec;
+
+#endif
+    }
+	unsigned int getTimeElapsedMS()//mil-seconds
+	{
+		return getGlobalTime() - _start_time;
+	}
+	void resetStartTime()
+	{
+		_start_time = getGlobalTime();
+	}
+	float getTimeElapsed()//seconds
+	{
+		return 0.001f*(getGlobalTime() - _start_time);
+	}
+
+	void profileFunction(char* funcName)
+	{
+#ifdef USING_FLO_WRITE
+		FloWrite("%s[%s] : %d ms\n", getProperBlank(), funcName, getTimeElapsedMS());
+#else
+		printf("%s[%s] : %d ms\n", getProperBlank(), funcName, getTimeElapsedMS());	
+#endif
+		resetStartTime();
+	}
+
+	static char* getProperBlank()
+	{
+		return "--";
+	}
+
+private:
+	unsigned int _start_time;
+};
+
+#endif
